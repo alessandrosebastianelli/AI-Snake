b25ed8b44cabac15416a6bf1396a8199963d7c3d
Sebbyraft <alessandroseba1995@gmail.com>
2020-07-28 16:29:49 +0200

    ⚙ Utilities

--- utils.py
+++ utils.py
@@ -1,11 +1,18 @@
 import pygame
 
+# Function that implements the eating action
 def eat(player, food, game):
+    # If the snake position is equal to a fruit position
     if player.x == food.x and player.y == food.y:
+        # Generate a new fruit
         food.update(game, player)
+        # Set the eaten flag to True
         player.eaten = True
+        # Increment the score counter by 1
         game.score = game.score + 1
 
+# Update the screen
 def update_screen():
     pygame.display.update()
+    # This is needed for Mac users
     pygame.event.get()
