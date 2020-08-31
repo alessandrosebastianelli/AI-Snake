85dee2b43e4fbacee6ba417e6f6c9d5872934cf4
Sebbyraft <alessandroseba1995@gmail.com>
2020-08-31 12:09:41 +0200

    ⚙ Utilities

--- utils.py
+++ utils.py
@@ -32,7 +32,7 @@ def plot_training_stats(array_counter, array_score, epochs):
     x = np.array([array_counter])[0]
     y = np.array([array_score])[0]
 
-    xvals = np.arange(0, epochs-1, 0.1) # np.linspace(0, len(y), len(y))
+    xvals = np.linspace(0, epochs, epochs)
     #yinterp = np.interp(xvals, x, y)
     f = interpolate.interp1d(x, y)
     yinterp = f(xvals)
